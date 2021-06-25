031c003b06fcda89475bb21193430defd1e8d355
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2021-06-25 21:11:43 +0500

    RAW Project SQL(DDL,DML) complete

---
+++ Shopping_Strom_DML(SQL)/SHoppingStromDML.sql
@@ -0,0 +1,408 @@
+USE ShoppingStrom;
+
+--TABLE Admin
+
+INSERT INTO Admin VALUES('AD001','Abdul','Majid','01-28-2001',21,'iam.admajid@gamil.com',
+						'iamMajid','house#7, Paktown, Chungi Amer Sidhu,lahore, Punjab','M','03424227313');
+INSERT INTO Admin VALUES('AD002','Waseem','Bajwa','01-01-1996',27,'iam.Waseem@gamil.com',
+						'iamWaseem','house#109, Mughal Pura lahore, Punjab','M','03160485774');
+INSERT INTO Admin VALUES('AD003','Yasmeen',NULL,'01-01-1985',36,'iam.Yasmeen@gamil.com',
+						'iamYasmeen','house#7, Paktown, Chungi Amer Sidhu,lahore, Punjab','F',NULL);
+TRUNCATE Table Admin; --Not working
+SELECT *FROM Admin;
+DELETE FROM Admin-- working
+
+--TABLE Customer
+
+INSERT INTO Customer VALUES('CU001','Abdul','Majid','01-28-2001',21,
+							'iam.admajid@gamil.com','iamAdMajid','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab','M','03424227313');
+
+INSERT INTO Customer VALUES('CU002','Jaime','Mill','12-22-1951',70,
+							'sjmuir@icloud.com','ax84r0ZEiADs','Street: 39/3, Off:Serai Road,
+							Karach Sindh','M','9221-2426077');
+
+INSERT INTO Customer VALUES('CU003','Joan','Mata','9-17-1960',61,
+							'thrymm@aol.com','oBQvKn3WPLXE','44 H-2 Wapda Town, 
+							P.O. Box: Lahore Punjab','M','92-42-5185571');
+
+INSERT INTO Customer VALUES('CU004','Piotr','Guerra','6-10-1961',59,
+							'firstpr@comcast.net','35bifZzl6q6L','Street:  #3, Pearl Plaza Near 
+							Shah Jamal Stop 174-Ferozepur,Lahore,
+							Punjab','M','9242-7567563');
+
+INSERT INTO Customer VALUES('CU005','Noa','Ellison','5-18-1962',58,
+							'jfmulder@att.net','W2LvFXOc8ZbB','Street:  B2 the 8th Floor,
+							Times Business Center,Jiyang Avenue,lZhuji,
+							China','M','87181728-898');
+
+INSERT INTO Customer VALUES('CU006','Ioan','Muhammad','2-21-1964',55,
+							'jonadab@sbcglobal.net','QCuC1yzlFht5','Street:  Sector 12-D,Karachi,
+							 Sindh','M',' 9221-6960523');
+
+INSERT INTO Customer VALUES('CU007','Devon','Majid','5-5-1964',55,
+							'msloan@att.net','8lB6lAMCUxaJ','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab','M','03424227313');
+
+INSERT INTO Customer VALUES('CU008','Abdul','Majid','8-12-1964',55,
+							'mbalazin@live.com','2R5T9TMxZmH7','45c SIE SIALKOT,
+							Punjab','M','03133552354');
+
+INSERT INTO Customer VALUES('CU009','Franky','Barnett','4-10-1972',49,
+							'jorgb@live.com','iTcciwuZF6AV','Chapal Plaza, Hasrat Mohani Road,
+							Karachi,Sindh','M','03565656565');
+
+INSERT INTO Customer VALUES('CU010','Karson','Hernandez','1-1-1976',45,
+							'seemant@live.com','57cZWMYj230p','Sector 24 Korangi Industrial Area,lahore,
+							Punjab','M','03121212212');
+
+INSERT INTO Customer VALUES('CU011','Manveer','Thornton','7-14-1976',45,
+							'gemmell@gmail.com','AbNIu4B3g1vt','Shop # 92, Saddar,lahore,
+							Punjab','M','03421111113');
+
+INSERT INTO Customer VALUES('CU012','Aronas','Cardenas','4-4-1982',39,
+							'dprice@gmail.com','84400sGVbNOC',' Heaven-1, Mubarak Pura,lahore,
+							Punjab','M','03424227313');
+
+INSERT INTO Customer VALUES('CU013','Akram','Heaton','02-28-2001',21,
+							'eidac@icloud.com','Lq0CU0Twhcpq','Raja Ghazanfar Ali Khan Road Saddar,lahore,
+							Punjab','M','03421117313');
+
+INSERT INTO Customer VALUES('CU014','Arslan','Palmer','9-4-1983',38,
+							'nichoj@att.net','6SoSelIPC3SH','65-Chenab Market Madina Town,lahore,
+							Punjab','M','03222227313');
+
+
+INSERT INTO Customer VALUES('CU015','Tadhg','Lewis','3-31-1986',35,
+							'kuparine@mac.com','14vkV4WI10sJ','26-b, Lalazar,lahore,
+							Punjab','M','03421111313');
+
+
+INSERT INTO Customer VALUES('CU016','Klara','Doherty','3-18-1991',32,
+							'becchi@comcast.net','J4sc2kKMNW3x','16/B, Committe Chowk,lahore,
+							Punjab','F','034244333313');
+
+INSERT INTO Customer VALUES('CU017','Adela','Talley','01-28-2001',20,
+							'nelson@yahoo.com','eGrZYM7Rh0Td','Saddar Sarmad Road,lahore,
+							Punjab','F','03424227313');
+
+INSERT INTO Customer VALUES('CU018','Husna','Alfaro','9-10-1961',81,
+							'lamky@optonline.net','t5e7uJwKdpEg','C/3 Gulshan-e-Behar, Orangi Town, Karachi,lahore'
+							,'F','03424235413');
+
+INSERT INTO Customer VALUES('CU019','Alannah','Lambert','5-2-2000',21,
+							'bogjobber@gmail.com','o0vzcg06BAau','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab','F','03478767313');
+
+INSERT INTO Customer VALUES('CU020','Ikrah','Byrd','6-26-2002',19,
+							'ngedmond@msn.com','Y2Jzkp0HJwbk','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab','F','03424227513');
+
+INSERT INTO Customer VALUES('CU021','Carly','Sosa','2-1-2005',15,
+							'mirod@gmail.com','UBYUQu6f5bAZ','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab','F','03424227413');
+
+INSERT INTO Customer VALUES('CU022','Ilayda','Majid','3-27-2008',12,
+							'ijackson@icloud.com','eUx4wiFgPUSG',' S.I.T.E,lahore,
+							Punjab','F','03422227313');
+
+INSERT INTO Customer VALUES('CU023','Alvarez','Majid','10-2-2008',12,
+							'ilial@live.com','7dmHS3xp6cJH',' Khanewal Road,lahore,
+							Punjab','F','0324327313');
+
+INSERT INTO Customer VALUES('CU024','Munib','Majid','7-22-2012',9,
+							'pierce@aol.com','QDOTfia8dw4T','Zaman Street, Chaklala,lahore,
+							Punjab','M','03412347313');
+
+INSERT INTO Customer VALUES('CU025','Janae','Ritter','2-12-2015',5,
+							'rhialto@yahoo.ca','3em3RJ8apm49','Khanewal Road,lahore,
+							Punjab','F','03425678313');
+
+SELECT * From Customer;
+
+--TABLE Categories
+
+INSERT INTO Categories VALUES ('CA001','Sneakers','Formal');
+INSERT INTO Categories VALUES ('CA002','Sneakers','InFormal');
+INSERT INTO Categories VALUES ('CA003','Sneakers','Casual');
+INSERT INTO Categories VALUES ('CA004','Linen Shirt','Formal');
+INSERT INTO Categories VALUES ('CA005','Linen Shirt','InFormal');
+INSERT INTO Categories VALUES ('CA006','Linen Shirt','Casual');
+INSERT INTO Categories VALUES ('CA007','Office Shirt','Formal');
+INSERT INTO Categories VALUES ('CA008','Flannel Shirt','Casual');
+INSERT INTO Categories VALUES ('CA009','Overshirt','InFormal');
+INSERT INTO Categories VALUES ('CA010','Dress Shirt','Formal');
+INSERT INTO Categories VALUES ('CA011','Dress Shirt','Casual');
+INSERT INTO Categories VALUES ('CA012','Check pants','Formal');
+INSERT INTO Categories VALUES ('CA013','Check pants','Casual');
+INSERT INTO Categories VALUES ('CA014','Dress pants','Formal');
+INSERT INTO Categories VALUES ('CA015','Dress pants','Casual');
+INSERT INTO Categories VALUES ('CA016','Slippers','InFormal');
+INSERT INTO Categories VALUES ('CA017','Sandals','Formal');
+INSERT INTO Categories VALUES ('CA018','Sandals','Casual');
+INSERT INTO Categories VALUES ('CA019','Sandals','InFormal');
+INSERT INTO Categories VALUES ('CA020','Loafers','InFormal');
+INSERT INTO Categories VALUES ('CA021','joggers','InFormal');
+INSERT INTO Categories VALUES ('CA022','Baggy Pants','InFormal');
+INSERT INTO Categories VALUES ('CA023','Jeans','Formal');
+INSERT INTO Categories VALUES ('CA024','Jeans','InFormal');
+INSERT INTO Categories VALUES ('CA025','Palazzo','InFormal');
+INSERT INTO Categories VALUES ('CA026','Leggings','InFormal');
+
+Select * From Categories;
+
+-- TABLE Product
+
+INSERT INTO Product Values ('PR001','Power - Men','Power','Blue','8','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industrys standard dummy text ever since the 1500s,',4999,'B','FootWear','10',
+'AD001','CA021');
+
+INSERT INTO Product Values ('PR002','Mens Stout Formal Shoes','Ndure','Brown','10',
+'An exquisite shoe for an exquisite look are what these Mens Stout Formal Shoes by Ndure are all about.
+These shoes have been made with good quality Man Made Leather material that gives them durability.'
+,1499,'M','FootWear','5','AD001','CA003');
+
+INSERT INTO Product Values ('PR003','Peep-Toe Heels','Ndure','Brown','07',
+'An exquisite shoe for an exquisite look are what these Formal by Ndure are all about.'
+,2799,'F','FootWear','3','AD001','CA018');
+
+INSERT INTO Product Values ('PR004','Cushioned Sandals for Women','Stylo','Purple','6','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',2999,'F','FootWear','20',
+'AD001','CA019');
+
+INSERT INTO Product Values ('PR005','Air Jordan 1 Mid','Nike','Tropical Twist','9','The Air Jordan 1 Mid Shoe is inspired by the first AJ1,
+offering fans of Jordan retros a chance to follow in the footsteps of greatness',14999,'M','FootWear','05',
+'AD001','CA002');
+
+INSERT INTO Product Values ('PR006','ULTRABOOST SUMMER.RDY TOKYO SHOES','Adidas','red-white','9','The Adidas Shoe is inspired by the first AJ1,
+offering fans of  retros a chance to follow in the footsteps of greatness',14999,'M','FootWear','03',
+'AD001','CA002');
+
+INSERT INTO Product Values ('PR007','White Color Formal Slippers','Stylo','white','7','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',1499,'F','FootWear','30',
+'AD001','CA016');
+
+INSERT INTO Product Values ('PR008','Maroon Color Fancy Sandals','Stylo','red','7','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',2999,'F','FootWear','15',
+'AD001','CA017');
+
+INSERT INTO Product Values ('PR009','Yellow Color Kids Girls sandal','Stylo','yellow','4','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',1299,'F','FootWear','50',
+'AD001','CA019');
+
+INSERT INTO Product Values ('PR010','Classy Chappals for Boys','chawla','balck','3','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',399,'M','FootWear','80',
+'AD001','CA020');
+
+INSERT INTO Product Values ('PR011','YELLOW FLANNEL CHECKERED SHIRT','Camel','yallow','M','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',2499,'M','Clothes','30',
+'AD002','CA010');
+
+INSERT INTO Product Values ('PR012','WHITE & BLUE GINGHAM FORMAL SHIRT','STyle','White','XL','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',5499,'M','Clothes','50',
+'AD002','CA010');
+
+INSERT INTO Product Values ('PR013','WHITE HERRINGBONE FORMAL SHIRT','J.','White','XXL','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',7499,'M','Clothes','50',
+'AD002','CA004');
+
+INSERT INTO Product Values ('PR014','KHAKI TWILL CHINO','J.','Khaki','34','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',5499,'M','Clothes','40',
+'AD003','CA015');
+
+INSERT INTO Product Values ('PR015','DARK BLUE SLIM FIT JEANS','J.','Blue','38','Lorem Ipsum is
+simply dummy text of the printing and typesetting industry.',3499,'B','Clothes','100',
+'AD003','CA023');
+
+Select * from Product;
+
+-- TABLE Images
+
+INSERT INTO Images Values ('PR001',
+'https://cdn.shopify.com/s/files/1/0143/1552/0054/products/828-9067-_1_720x.jpg?v=1623331275');
+
+INSERT INTO Images Values ('PR001',
+'https://cdn.shopify.com/s/files/1/0143/1552/0054/products/828-9067-_4_2400x.jpg?v=1623331276');
+
+INSERT INTO Images Values ('PR002',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…b120-0a62-48d4-a9aa-fd6dcf85a31f.jpg?v=1610610332');
+
+INSERT INTO Images Values ('PR002',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…cb81-aeb9-4417-9294-9034659f5873.jpg?v=1610610332');
+
+INSERT INTO Images Values ('PR003',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…89ae-0929-470c-8131-94df821a3486.jpg?v=1614247760');
+
+INSERT INTO Images Values ('PR003',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…3f04-c6f4-4980-9b98-263b0eeab639.jpg?v=1614247760');
+
+INSERT INTO Images Values ('PR004',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…1c77-ea48-4fb1-a304-374ecd89b400.jpg?v=1614143776');
+
+INSERT INTO Images Values ('PR004',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…93ec-d73e-4162-8c1b-f149d8b9118d.jpg?v=1614143777');
+
+INSERT INTO Images Values ('PR005',
+'https://static.nike.com/a/images/t_PDP_1280_v1/f_a…31e-10c00d6867d9/air-jordan-1-mid-shoe-vnwwTL.png');
+
+INSERT INTO Images Values ('PR005',
+'https://static.nike.com/a/images/t_PDP_1280_v1/f_a…53b-87afd15968ee/air-jordan-1-mid-shoe-vnwwTL.png');
+
+INSERT INTO Images Values ('PR006',
+'https://assets.adidas.com/images/w_600,f_auto,q_au…ltraboost_SUMMER.RDY_Tokyo_Shoes_White_FX0031.jpg');
+
+INSERT INTO Images Values ('PR007',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/FR762709_3_800x.jpg?v=1622784189');
+
+INSERT INTO Images Values ('PR007',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/FR762709_2_800x.jpg?v=1622784189');
+
+INSERT INTO Images Values ('PR008',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/FN477205_3_800x.jpg?v=1622182142');
+
+INSERT INTO Images Values ('PR008',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/FN477205_2_800x.jpg?v=1622182142');
+
+INSERT INTO Images Values ('PR009',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/KD649420_3_800x.jpg?v=1623219551');
+
+INSERT INTO Images Values ('PR009',
+'https://cdn.shopify.com/s/files/1/0485/1459/7030/products/KD649420_2_500x.jpg?v=1623219551');
+
+INSERT INTO Images Values ('PR010',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…af36-c94a-4a8c-948d-eb2b10eeb4d4.jpg?v=1611049199');
+
+INSERT INTO Images Values ('PR010',
+'https://cdn.shopify.com/s/files/1/0371/5416/0772/p…fc93-5397-4c9b-bd63-3939c22ebfba.jpg?v=1611049199');
+
+INSERT INTO Images Values ('PR011',
+'https://brumano.b-cdn.net/wp-content/uploads/2021/…ow-Flannel-Checkered-Shirt-Brumano-Pakistan-4.jpg');
+
+INSERT INTO Images Values ('PR012',
+'https://brumano.b-cdn.net/wp-content/uploads/2021/…ngham-Formal-Shirt-Brumano-Pakistan-1-625x833.jpg');
+
+INSERT INTO Images Values ('PR012',
+'https://brumano.b-cdn.net/wp-content/uploads/2021/…te-Blue-Gingham-Formal-Shirt-Brumano-Pakistan.jpg');
+
+INSERT INTO Images Values ('PR013',
+'https://brumano.b-cdn.net/wp-content/uploads/2020/09/Dark-Blue-Slim-Fit-Jeans-for-mens-min.jpg');
+
+INSERT INTO Images Values ('PR014',
+'https://brumano.b-cdn.net/wp-content/uploads/2021/06/Khaki-Twill-Chino-Brumano-Pakistan-7.jpg');
+
+INSERT INTO Images Values ('PR015',
+'https://brumano.b-cdn.net/wp-content/uploads/2020/09/Dark-Blue-Slim-Fit-Jeans-for-mens-min.jpg');
+
+Select * from Images;
+
+
+
+-- TABLE Cart
+
+INSERT INTO Cart VALUES('CI001','CU001');
+INSERT INTO Cart VALUES('CI002','CU002');
+INSERT INTO Cart VALUES('CI003','CU003');
+INSERT INTO Cart VALUES('CI004','CU004');
+INSERT INTO Cart VALUES('CI005','CU005');
+INSERT INTO Cart VALUES('CI006','CU006');
+INSERT INTO Cart VALUES('CI007','CU007');
+INSERT INTO Cart VALUES('CI008','CU008');
+INSERT INTO Cart VALUES('CI009','CU009');
+INSERT INTO Cart VALUES('CI010','CU010');
+INSERT INTO Cart VALUES('CI011','CU011');
+INSERT INTO Cart VALUES('CI012','CU012');
+INSERT INTO Cart VALUES('CI013','CU013');
+INSERT INTO Cart VALUES('CI014','CU014');
+INSERT INTO Cart VALUES('CI015','CU015');
+INSERT INTO Cart VALUES('CI016','CU016');
+INSERT INTO Cart VALUES('CI017','CU017');
+INSERT INTO Cart VALUES('CI018','CU018');
+INSERT INTO Cart VALUES('CI019','CU019');
+INSERT INTO Cart VALUES('CI020','CU020');
+INSERT INTO Cart VALUES('CI021','CU021');
+INSERT INTO Cart VALUES('CI022','CU022');
+INSERT INTO Cart VALUES('CI023','CU023');
+INSERT INTO Cart VALUES('CI024','CU024');
+INSERT INTO Cart VALUES('CI025','CU025');
+
+-- TABLE Cart_Product
+
+INSERT INTO Cart_Product VALUES('CI001','PR001');
+INSERT INTO Cart_Product VALUES('CI001','PR005');
+INSERT INTO Cart_Product VALUES('CI002','PR010');
+INSERT INTO Cart_Product VALUES('CI001','PR015');
+INSERT INTO Cart_Product VALUES('CI003','PR011');
+INSERT INTO Cart_Product VALUES('CI003','PR015');
+INSERT INTO Cart_Product VALUES('CI005','PR011');
+INSERT INTO Cart_Product VALUES('CI010','PR001');
+INSERT INTO Cart_Product VALUES('CI015','PR012');
+INSERT INTO Cart_Product VALUES('CI025','PR003');
+INSERT INTO Cart_Product VALUES('CI011','PR004');
+INSERT INTO Cart_Product VALUES('CI018','PR005');
+INSERT INTO Cart_Product VALUES('CI012','PR011');
+INSERT INTO Cart_Product VALUES('CI006','PR013');
+INSERT INTO Cart_Product VALUES('CI019','PR010');
+INSERT INTO Cart_Product VALUES('CI020','PR004');
+
+SELECT * FROM Cart_Product
+
+SELECT Cart_ID, SUM(Product.Price) from Cart_Product INNER Join Product On Cart_Product.Product_ID = Product.Product_ID
+Group by Cart_ID 
+
+-- TABLE Reviews
+
+INSERT INTO Reviews VALUES('RE001','PR001','CU001',1);
+INSERT INTO Reviews VALUES('RE002','PR001','CU010',2);
+INSERT INTO Reviews VALUES('RE003','PR011','CU003',3);
+INSERT INTO Reviews VALUES('RE004','PR011','CU005',4);
+INSERT INTO Reviews VALUES('RE005','PR005','CU018',5);
+INSERT INTO Reviews VALUES('RE006','PR012','CU015',4);
+INSERT INTO Reviews VALUES('RE006','PR012','CU001',5);
+
+-- TABLE Card_Info
+
+INSERT INTO Card_Info VALUES('4024 1467 8773 5427','Bank','CU001');
+INSERT INTO Card_Info VALUES('03424227313','EasyPaisa','CU010');
+INSERT INTO Card_Info VALUES('4116 9740 5626 8401','Bank','CU003');
+INSERT INTO Card_Info VALUES('03014595086','JazzCssh','CU005');
+INSERT INTO Card_Info VALUES('4475 1161 8263 3117','Bank','CU018');
+INSERT INTO Card_Info VALUES('4672 4934 4211 8556','Bank','CU015');
+INSERT INTO Card_Info VALUES('03424227433','EasyPaisa','CU011');
+INSERT INTO Card_Info VALUES('4437 4819 6805 2776','Bank','CU012');
+INSERT INTO Card_Info VALUES('03014595012','JazzCssh','CU006');
+INSERT INTO Card_Info VALUES('4323 8625 7313 1427','Bank','CU019');
+INSERT INTO Card_Info VALUES('4499 1170 7243 1846','Bank','CU020');
+
+-- TABLE Orders
+
+INSERT INTO Orders VALUES('OR001','house#7, Paktown, Chungi Amer Sidhu,lahore,
+							Punjab',23497,'CI001','4024 1467 8773 5427');
+INSERT INTO Orders VALUES('OR002','Sector 24 Korangi Industrial Area,lahore,
+							Punjab',4999,'CI010','03424227313');
+INSERT INTO Orders VALUES('OR003','44 H-2 Wapda Town, 
+							P.O. Box: Lahore Punjab',5998,'CI003','4116 9740 5626 8401');
+INSERT INTO Orders VALUES('OR004','Street:  B2 the 8th Floor,
+							Times Business Center,Jiyang Avenue,lZhuji,
+							China street',2499,'CI005','03014595086');
+INSERT INTO Orders VALUES('OR005','C/3 Gulshan-e-Behar, Orangi Town, Karachi,lahore'
+							,14999,'CI018','4475 1161 8263 3117');
+INSERT INTO Orders VALUES('OR006','26-b, Lalazar,lahore,
+							Punjab',5499,'CI015','4672 4934 4211 8556');
+INSERT INTO Orders VALUES('OR007','Shop # 92, Saddar,lahore,
+							Punjab',2999,'CI011','03424227433');
+INSERT INTO Orders VALUES('OR008',' Heaven-1, Mubarak Pura,lahore,
+							Punjab',2499,'CI012','4437 4819 6805 2776');
+INSERT INTO Orders VALUES('OR009','Street:  Sector 12-D,Karachi,
+							 Sindh',7499,'CI006','03014595012');
+INSERT INTO Orders VALUES('OR010','Comsats Uni Lahore pakistan',399,'CI019','4323 8625 7313 1427');
+INSERT INTO Orders VALUES('OR011','house#7,
+							Paktown, Chungi Amer Sidhu,lahore,
+							Punjab',2999,'CI020','4499 1170 7243 1846');
+
+Select * FROM Orders;
